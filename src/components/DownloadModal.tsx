187b975375912aebbce51830b44b5dbb935747d2
Barkshihun <barkshihun2@gmail.com>
2022-11-13 01:12:36 +0900

    💄 Style: 다운로드 모달창 headless ui로 변경 DownloadModal로 컴포넌트 분리

---
+++ src/components/DownloadModal.tsx
@@ -0,0 +1,87 @@
+import { Fragment, useRef, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { ArrowDownTrayIcon } from "@heroicons/react/24/outline";
+
+function DownloadModal({ open, setOpen, skinPngCanvas }: { open: boolean; setOpen: React.Dispatch<React.SetStateAction<boolean>>; skinPngCanvas: HTMLCanvasElement }) {
+  const DEFAULT_TEXT = "랜덤 색깔 스킨";
+  const [text, setText] = useState("");
+  const downloadBtnRef = useRef(null);
+  const onDownload = () => {
+    const dataURL = skinPngCanvas.toDataURL();
+    const aTag = document.createElement("a");
+    aTag.download = `${text === "" ? DEFAULT_TEXT : text}.png`;
+    aTag.href = dataURL;
+    aTag.click();
+    setOpen(false);
+  };
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={setOpen} initialFocus={downloadBtnRef}>
+        <Transition.Child as={Fragment} enter="ease-out duration-200" enterFrom="opacity-0" enterTo="opacity-100" leave="ease-in duration-200" leaveFrom="opacity-100" leaveTo="opacity-0">
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-200"
+              enterFrom="opacity-0 translate-y-4 "
+              enterTo="opacity-100 translate-y-0 "
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 s"
+              leaveTo="opacity-0 translate-y-4 "
+            >
+              <Dialog.Panel className="w-[50%] relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all">
+                <div className="bg-white px-4 pt-5 pb-4 ">
+                  <div className="">
+                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-blue-100 ">
+                      <ArrowDownTrayIcon className="h-6 w-6 text-blue-600" aria-hidden="true" />
+                    </div>
+                    <div className="mt-3 text-center">
+                      <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
+                        파일을 다운로드하시겠습니까?
+                      </Dialog.Title>
+                      <h4 className="mt-1 text-gray-400 ">파일명: {text === "" ? DEFAULT_TEXT : text}.png</h4>
+                      <input
+                        className="w-[80%] mt-2 pl-5 py-1 rounded-md border border-gray-300 focus:outline-none  focus:ring-1 focus:ring-gray-500"
+                        type="text"
+                        placeholder={DEFAULT_TEXT}
+                        value={text}
+                        onChange={({ target: { value } }) => {
+                          setText(value);
+                        }}
+                        onKeyDown={({ key }) => {
+                          if (key === "Enter") {
+                            onDownload();
+                          }
+                        }}
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-gray-50 px-4 py-3 flex justify-evenly">
+                  <button
+                    type="button"
+                    className="flex w-2/5 justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
+                    onClick={() => setOpen(false)}
+                  >
+                    취소
+                  </button>
+                  <button
+                    type="button"
+                    className="flex w-2/5 justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    onClick={onDownload}
+                    ref={downloadBtnRef}
+                  >
+                    다운로드
+                  </button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  );
+}
+export default DownloadModal;
--- src/components/RenderedSkin.tsx
+++ src/components/RenderedSkin.tsx
@@ -2,12 +2,12 @@ import { useEffect, useRef, useState } from "react";
 import { useDispatch } from "react-redux";
 import { undo } from "../store/isGeneratedSlice";
 import { SkinViewer } from "skinview3d";
-import Swal from "sweetalert2";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShirt } from "@fortawesome/free-solid-svg-icons";
-import withReactContent from "sweetalert2-react-content";
+import DownloadModal from "./DownloadModal";
 
 function RenderedSkin({ imageData, noOverlayImageData }: { imageData: ImageData; noOverlayImageData: ImageData }) {
+  const [open, setOpen] = useState(false);
   const [isOverlay, setOverlay] = useState(true);
   const dispatch = useDispatch();
   const skinPngCanvasRef = useRef<HTMLCanvasElement>(null);
@@ -33,40 +33,9 @@ function RenderedSkin({ imageData, noOverlayImageData }: { imageData: ImageData;
   const onLayoutBtnClick = () => {
     setOverlay((prevState) => !prevState);
   };
-  const onDownload = () => {
-    let inputValue = "랜덤 색깔 스킨";
-    const MySwal = withReactContent(Swal);
-    const html = (
-      <>
-        <span id="swal">파일명: 랜덤 색깔 스킨.png</span>
-        <br />
-        <input
-          type="text"
-          onChange={({ target: { value } }) => {
-            const span = document.getElementById("swal") as HTMLElement;
-            inputValue = value === "" ? "랜덤 색깔 스킨" : value;
-            span.innerText = `파일명: ${inputValue}.png`;
-          }}
-        />
-      </>
-    );
-    MySwal.fire({
-      title: "파일을 다운로드하시겠습니까?",
-      html,
-      inputValue: "랜덤 색깔 스킨",
-      showCancelButton: true,
-    }).then((result) => {
-      if (result.isConfirmed) {
-        const dataURL = skinPngCanvas.toDataURL();
-        const aTag = document.createElement("a");
-        aTag.download = `${inputValue}.png`;
-        aTag.href = dataURL;
-        aTag.click();
-      }
-    });
-  };
   return (
     <section className="mt-14">
+      <DownloadModal open={open} setOpen={setOpen} skinPngCanvas={skinPngCanvasRef.current as HTMLCanvasElement} />
       <div className="flex flex-col h-[70%] justify-evenly items-center">
         <canvas className="shadow-2xl" ref={skinCanvasRef} width={200} height={200}></canvas>
         <FontAwesomeIcon icon={faShirt} onClick={onLayoutBtnClick} style={{ color: isOverlay ? "#ff5c5c" : "#707070" }} />
@@ -80,7 +49,12 @@ function RenderedSkin({ imageData, noOverlayImageData }: { imageData: ImageData;
       >
         돌아가기
       </button>
-      <button onClick={onDownload} className="bg-blue-300 mb-2">
+      <button
+        onClick={() => {
+          setOpen(true);
+        }}
+        className="bg-blue-300 mb-2"
+      >
         다운로드
       </button>
     </section>
