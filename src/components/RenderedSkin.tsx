01c9ce5169cb1cd56cde8aa4ec703eac8bdc1535
Barkshihun <barkshihun2@gmail.com>
2022-11-15 00:26:13 +0900

    💄 Style: 모바일에서 렉 걸려서 canvas의 트렌지션 삭제

--- src/components/RenderedSkin.tsx
+++ src/components/RenderedSkin.tsx
@@ -5,7 +5,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShirt } from "@fortawesome/free-solid-svg-icons";
 import { ArrowUturnLeftIcon } from "@heroicons/react/24/outline";
 import DownloadModal from "./DownloadModal";
-import { CSSTransition } from "react-transition-group";
 
 function RenderedSkin({ imageData, noOverlayImageData }: { imageData: ImageData; noOverlayImageData: ImageData }) {
   const [open, setOpen] = useState(false);
@@ -43,9 +42,7 @@ function RenderedSkin({ imageData, noOverlayImageData }: { imageData: ImageData;
       <DownloadModal open={open} setOpen={setOpen} skinPngCanvas={skinPngCanvasRef.current as HTMLCanvasElement} />
       <div className="h-full flex flex-col items-center">
         <div className="flex flex-col h-[70%] max-h-[500px] justify-evenly items-center">
-          <CSSTransition nodeRef={skinCanvasRef} in={isOverlay} classNames="canvas-load" timeout={500}>
-            <canvas ref={skinCanvasRef} className="shadow-2xl bg-white rounded-md animate-init-canvas-load" width={200} height={200}></canvas>
-          </CSSTransition>
+          <canvas ref={skinCanvasRef} className="shadow-2xl bg-white rounded-md animate-init-canvas-load" width={200} height={200}></canvas>
           <button className="rounded-full w-[40px] h-[40px] flex items-center justify-center">
             <FontAwesomeIcon
               icon={faShirt}
@@ -54,9 +51,7 @@ function RenderedSkin({ imageData, noOverlayImageData }: { imageData: ImageData;
               className="h-[30px] w-[30px] drop-shadow transition hover:scale-105 hover:brightness-150"
             />
           </button>
-          <CSSTransition nodeRef={skinPngCanvasRef} in={isOverlay} classNames="canvas-load" timeout={500}>
-            <canvas ref={skinPngCanvasRef} className="shadow-2xl w-40 h-40 bg-white rounded-md p-3 animate-init-canvas-load" width={64} height={64}></canvas>
-          </CSSTransition>
+          <canvas ref={skinPngCanvasRef} className="shadow-2xl w-40 h-40 bg-white rounded-md p-3 animate-init-canvas-load" width={64} height={64}></canvas>
         </div>
         <div className="flex justify-evenly w-[200px] mt-4">
           <button
